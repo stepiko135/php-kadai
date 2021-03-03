af6a275e668fb56387f20fe2d6550a283ba7a56e
HIRO <stepiko135@gmail.com>
2021-03-03 11:27:18 +0900

    課題①-4

--- index.php
+++ index.php
@@ -4,26 +4,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>1-2</title>
+    <title>1-4</title>
 </head>
 
 <body>
+    <form action="" method="post">
+        <select name="janken">
+            <option value="グー">グー</option>
+            <option value="チョキ">チョキ</option>
+            <option value="パー">パー</option>
+        </select>
+        <br>
+        <input type="submit" value="じゃんけん！">
+    </form>
     <?php
-    $questions = [
-        "問題" => "日本の首都は？",
-    ];
-    $questionsKeys = array_keys($questions);
+    function janken($user)
+    {
+        $choice = ["グー", "チョキ", "パー"];
+        $com = $choice[rand(0, 2)];
+        $user = $user;
 
-    $answers = [
-        "回答1" => "大阪",
-        "回答2" => "北海道",
-        "回答3" => "箱根",
-        "回答4" => "東京",
-    ];
+        echo '自分:' . $user .'<br>';
+        echo '相手:' . $com .'<br>';
+        if ($user === $com) {
+            echo "あいこ";
+        } elseif ($user === "グー" && $com === "チョキ") {
+            echo "あなたの勝ち！";
+        } elseif ($user === "グー" && $com === "パー") {
+            echo "あなたの敗北です。。。";
+        } elseif ($user === "チョキ" && $com === "グー") {
+            echo "あなたの敗北です。。。";
+        } elseif ($user === "チョキ" && $com === "パー") {
+            echo "あなたの勝ち！";
+        } elseif ($user === "パー" && $com === "グー") {
+            echo "あなたの勝ち！";
+        } elseif ($user === "パー" && $com === "チョキ") {
+            echo "あなたの敗北です。。。";
+        }
+    };
 
-    echo "<h1>{$questionsKeys[0]} {$questions["問題"]} </h1>";
-    foreach ($answers as $key => $answer) {
-        echo "{$key} {$answer}<br>";
+    if (isset($_POST['janken'])) {
+        janken($_POST['janken']);
     }
     ?>
 </body>
