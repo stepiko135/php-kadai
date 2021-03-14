c529959e23ddc2694c00dd47614db04a93863717
HIRO <stepiko135@gmail.com>
2021-03-15 08:28:29 +0900

    try/catch 追加

--- posted.php
+++ posted.php
@@ -10,13 +10,20 @@
 <body>
     <h2>投稿が完了しました。</h2>
     <button type="button" onclick="location.href='index.php'">投稿一覧へ戻る</button>
+    <br>
+    <br>
+    <?php
+    try {
+        $mySql = new PDO('mysql:host=localhost:3306;dbname=kadai', 'root', 'root');
+
+        $stmt = $mySql->prepare("INSERT INTO posts (name, content) VALUES (:name, :content)");
+        $stmt->bindParam(':name', $_POST['name']);
+        $stmt->bindParam(':content', $_POST['content']);
+        $stmt->execute();
+    } catch (Exception $e) {
+        echo "エラーが発生しました。: {$e->getMessage()}";
+    }
+    ?>
 </body>
 
 </html>
-<?php
-$mySql = new PDO('mysql:host=localhost:3306;dbname=kadai', 'root', 'root');
-
-$stmt = $mySql->prepare("INSERT INTO posts (name, content) VALUES (:name, :content)");
-$stmt->bindParam(':name', $_POST['name']);
-$stmt->bindParam(':content', $_POST['content']);
-$stmt->execute();
