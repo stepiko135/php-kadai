f51e14c050ef87dd2a69d1bcd57c8b37983cb710
HIRO <stepiko135@gmail.com>
2021-03-15 10:32:52 +0900

    課題③-2

--- index.php
+++ index.php
@@ -29,14 +29,21 @@ $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </form>
     <br>
     <h2>投稿内容一覧</h2>
-    <?php foreach($posts as $post): ?>
-    <div style="border: double 5px cyan">
-    <?= "No: " . $post['id'] . "<br>"?>
-    <?= "名前: ". $post['name'] . "<br>"?>
-    <?= "投稿内容: " . $post['content'] . "<br>"?>
-    </div>
-    <br>
-    <?php endforeach;?>
+    <?php $count = 1 ?>
+    <?php foreach ($posts as $post) : ?>
+        <div style="border: double 5px cyan">
+            <?= "No: " . $count . "<br>" ?>
+            <?= "名前: " . $post['name'] . "<br>" ?>
+            <?= "投稿内容: " . $post['content'] . "<br>" ?>
+            <form action="deleted.php" method="POST">
+                <input type="hidden" name="method" value="DELETE">
+                <?= '<input type="hidden" name="id" value=' . $post['id'] . '>' ?>
+                <input type="submit" value="削除">
+            </form>
+        </div>
+        <br>
+    <?php $count++ ?>
+    <?php endforeach; ?>
 </body>
 
 </html>
