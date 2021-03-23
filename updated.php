5a245f89709d92abc03a504d01c468865d3ea798
HIRO <stepiko135@gmail.com>
2021-03-24 01:37:39 +0900

    ③-3php記述位置の修正

--- updated.php
+++ updated.php
@@ -1,3 +1,17 @@
+    <?php
+    try {
+        $mySql = new PDO('mysql:host=localhost:3306;dbname=kadai', 'root', 'root');
+
+        $stmt = $mySql->prepare("UPDATE posts SET name = :name, content = :content WHERE id = :id");
+        $stmt->bindParam(':id', $_POST['id']);
+        $stmt->bindParam(':name', $_POST['name']);
+        $stmt->bindParam(':content', $_POST['content']);
+        $stmt->execute();
+    } catch (Exception $e) {
+        echo "エラーが発生しました。: {$e->getMessage()}";
+    }
+    ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -12,19 +26,6 @@
     <button type="button" onclick="location.href='index.php'">投稿一覧へ戻る</button>
     <br>
     <br>
-    <?php
-    try {
-        $mySql = new PDO('mysql:host=localhost:3306;dbname=kadai', 'root', 'root');
-
-        $stmt = $mySql->prepare("UPDATE posts SET name = :name, content = :content WHERE id = :id");
-        $stmt->bindParam(':id', $_POST['id']);
-        $stmt->bindParam(':name', $_POST['name']);
-        $stmt->bindParam(':content', $_POST['content']);
-        $stmt->execute();
-    } catch (Exception $e) {
-        echo "エラーが発生しました。: {$e->getMessage()}";
-    }
-    ?>
 </body>
 
 </html>
